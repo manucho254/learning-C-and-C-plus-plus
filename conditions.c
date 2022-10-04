8e7c252f3856a8de2a3782579f902e95f96f9395
Emmanuel manucho <rampagetest254@gmail.com>
2022-10-04 18:34:57 +0300

    conditions

---
+++ conditions.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+ if 
+
+ else if 
+
+ else
+
+*/
+
+int main (void) {
+
+    int grade = 0;
+
+    printf("Grade: ");
+    scanf("%d", &grade);
+
+    if (grade >= 50) printf("Passed \n");
+
+    else printf("avarage \n");
+
+    return 0;
+}
+
